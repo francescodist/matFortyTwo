2eff0f176f668c82a3a0b292c39a88a638aad113
Keenan Johnson <keenan.johnson1@outlook.com>
2019-06-01 12:33:06 -0400

    Write unit test for setActivePage method

--- src/app/core/services/navigation/navigation.service.spec.ts
+++ src/app/core/services/navigation/navigation.service.spec.ts
@@ -2,14 +2,12 @@ import { TestBed } from '@angular/core/testing';
 
 import { NavigationService } from './navigation.service';
 
-import { Page } from './navigation.service';
-
 fdescribe('NavigationService', () => {
     let service: NavigationService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [NavigationService]
+            providers: [NavigationService],
         });
         service = TestBed.get(NavigationService);
     });
@@ -18,14 +16,22 @@ fdescribe('NavigationService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should get the corret navigationItems', () => {
+    it('should get the correct navigationItems', () => {
         const mockNavRouteItems = [
             { path: 'somePath' },
             { path: 'somePath2' },
-            { path: 'somePath3' }
+            { path: 'somePath3' },
         ];
         // tslint:disable-next-line:no-string-literal
         service['navigationItems'] = mockNavRouteItems;
         expect(service.getNavigationItems()).toEqual(mockNavRouteItems);
+    });
+
+    it('should set the activePage', () => {
+        service.setActivePage('fakeTitle', true);
+        // tslint:disable-next-line:no-string-literal
+        const activePage = service['activePage'];
+        expect(activePage.title).toEqual('fakeTitle');
+        expect(activePage.isChild).toEqual(true);
     });
 });
