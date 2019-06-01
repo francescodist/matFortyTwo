7c649bf3e159228d5f4524e9bf9aac63e74db5c1
Keenan Johnson <keenan.johnson1@outlook.com>
2019-06-01 12:12:40 -0400

    Add unit test for the getNavigationItems method

--- src/app/core/services/navigation/navigation.service.spec.ts
+++ src/app/core/services/navigation/navigation.service.spec.ts
@@ -2,11 +2,30 @@ import { TestBed } from '@angular/core/testing';
 
 import { NavigationService } from './navigation.service';
 
-describe('NavigationService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+import { Page } from './navigation.service';
+
+fdescribe('NavigationService', () => {
+    let service: NavigationService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [NavigationService]
+        });
+        service = TestBed.get(NavigationService);
+    });
 
     it('should be created', () => {
-        const service: NavigationService = TestBed.get(NavigationService);
         expect(service).toBeTruthy();
+    });
+
+    it('should get the corret navigationItems', () => {
+        const mockNavRouteItems = [
+            { path: 'somePath' },
+            { path: 'somePath2' },
+            { path: 'somePath3' }
+        ];
+        // tslint:disable-next-line:no-string-literal
+        service['navigationItems'] = mockNavRouteItems;
+        expect(service.getNavigationItems()).toEqual(mockNavRouteItems);
     });
 });
